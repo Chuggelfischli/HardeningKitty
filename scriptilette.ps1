052ada28406d1bc5b6c85c6bb0f2e04452e0a69a
Chuggelfischli <104386229+Chuggelfischli@users.noreply.github.com>
2025-01-28 12:44:17 +0100

    Update scriptilette.ps1

--- scriptilette.ps1
+++ scriptilette.ps1
@@ -3,20 +3,19 @@ $currentPath = Get-Location
 ## Quelldatei einlesen - Log File welches die Auszuwertenden Alerts enthält
 $source = Read-Host "Geben Sie die Quelldatei inkl. vollstaendigen Pfad an, ohne Anfuehrungszeichen"
 
-$destination = "$currentPath\High_Medium_Low_Severity_Alerts.log"
-
-
 # Computernamen abrufen
 $computerName = $env:COMPUTERNAME
 
+$destination = "$currentPath\$computerName-High_Medium_Low_Severity_Alerts.log"
+
 # Computernamen in die Zieldatei schreiben
-"Computername: $computerName" | Set-Content $destination
+"Computername:" + $computerName | Set-Content $destination
 
 # Inhalt der Quelldatei lesen und filtern
 $getFilteredRows = Get-Content $source | Where-Object { $_ -match "(High|Medium|Low)$" }
 
 # Gefilterte Zeilen in die Zieldatei schreiben
-$getFilteredRows | Set-Content $destination
+$getFilteredRows | add-Content $destination
 
 # Bestätigung ausgeben
 Write-Host "Die gefilterte Zeilen wurden in $destination kopiert. Have a great Day ;D"
