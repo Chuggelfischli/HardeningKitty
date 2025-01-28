d3bdb40b5de5bcd6ee3a9999a7e7eb9c7ec7d88d
Chuggelfischli <104386229+Chuggelfischli@users.noreply.github.com>
2025-01-28 10:28:05 +0100

    Add files via upload

---
+++ scriptilette.ps1
@@ -0,0 +1,22 @@
+# Pfade für Quell- und Zieldatei definieren
+$currentPath = Get-Location
+## Quelldatei einlesen - Log File welches die Auszuwertenden Alerts enthält
+$source = Read-Host "Geben Sie die Quelldatei inkl. vollstaendigen Pfad an, ohne Anfuehrungszeichen"
+
+$destination = "$currentPath\High_Medium_Low_Severity_Alerts.log"
+
+
+# Computernamen abrufen
+$computerName = $env:COMPUTERNAME
+
+# Computernamen in die Zieldatei schreiben
+"Computername: $computerName" | Set-Content $destination
+
+# Inhalt der Quelldatei lesen und filtern
+$getFilteredRows = Get-Content $source | Where-Object { $_ -match "(High|Medium|Low)$" }
+
+# Gefilterte Zeilen in die Zieldatei schreiben
+$getFilteredRows | Set-Content $destination
+
+# Bestätigung ausgeben
+Write-Host "Die gefilterte Zeilen wurden in $destination kopiert. Have a great Day ;D"
